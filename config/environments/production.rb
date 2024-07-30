49682fc64f9b9ac24f104aed002f577e9757332d
karim <karimloco4@gmail.com>
2024-07-30 22:27:39 +0100

    fixing mail hog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,17 +87,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'yourdomain.com', # Replace with your domain
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
-
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
 end
